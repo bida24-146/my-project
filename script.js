8ccdcc877cf8971849cf7ad310264cf1132b19b7
bida24-146 <bida24-146@thuto.bac.ac.bw>
2026-04-16 16:42:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function showMessage(product) {
+    alert(product + " added to cart 🛒");
+}
 
+/* Simple futuristic text animation */
+window.onload = () => {
+    const title = document.querySelector(".hero h2");
+    if (title) {
+        let text = title.innerText;
+        title.innerText = "";
+
+        let i = 0;
+        let interval = setInterval(() => {
+            title.innerText += text[i];
+            i++;
+            if (i === text.length) clearInterval(interval);
+        }, 50);
+    }
+};
